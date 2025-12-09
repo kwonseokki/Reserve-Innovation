cc89dd4f3fe2de93ff86507fac77d7e126d5d0f8
권석기 <kwons3327@gmail.com>
2025-12-10 01:53:26 +0900

    feat: 나의 훈련정보 불러오기 기능 추가

---
+++ FindReserve/Presentation/Matching/ViewModel/TrainingSelectionViewModel.swift
@@ -0,0 +1,70 @@
+//
+//  TrainingSelectionViewModel.swift
+//  FindReserve
+//
+//  Created by a on 12/10/25.
+//
+
+import Foundation
+import Combine
+import SwiftData
+
+class TrainingSelectionViewModel: ObservableObject {
+    private var modelContext: ModelContext
+    
+    private var trainingInfoID: UUID?
+    /// 훈련 종류
+    @Published var selectedTrainingType: String = ""
+    /// 훈련장
+    @Published var selectedTrainingCenter: String = ""
+    /// 훈련 종류 보임여부
+    @Published var trainingDropDownIsOpen: Bool = false
+    /// 훈련장 종류 보임여부
+    @Published var trainingCenterDropDownIsOpen: Bool = false
+    /// 훈련 종류 리스트
+    @Published var trainingTypes: [TrainingType] = TrainingType.allCases
+    /// 훈련장 종류 리스트
+    @Published var trainingCenters: [String] = []
+    /// 시작날짜
+    @Published var startDate: Date = Date()
+    
+    init(modelContext: ModelContext) {
+        self.modelContext = modelContext
+    }
+    
+    func fetchTrainingInfo() {
+        do {
+            let trainingList = try modelContext.fetch(FetchDescriptor<TrainingInfo>())
+            if let myTrainingInfo = trainingList.first {
+                selectedTrainingType = myTrainingInfo.trainingTypeValue
+                trainingInfoID = myTrainingInfo.id
+                selectedTrainingCenter = myTrainingInfo.departure
+                startDate = myTrainingInfo.startDate
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func saveTrainingInfo() {
+        do {
+            let trainingList = try modelContext.fetch(FetchDescriptor<TrainingInfo>())
+            if let myTrainingInfo = trainingList.first {
+                myTrainingInfo.trainingTypeValue = selectedTrainingType
+                myTrainingInfo.startDate = startDate
+                myTrainingInfo.departure = selectedTrainingCenter
+            } else {
+                let trainingInfo = TrainingInfo(
+                    id: trainingInfoID ?? UUID(),
+                    trainingTypeValue: selectedTrainingType,
+                    startDate: startDate,
+                    departure: selectedTrainingCenter
+                )
+                modelContext.insert(trainingInfo)
+            }
+            try modelContext.save()
+        } catch {
+            
+        }
+    }
+}
