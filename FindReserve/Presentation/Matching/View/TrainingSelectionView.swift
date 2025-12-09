6544aa5846b3016f79d69dd0aba6d256abdb3867
권석기 <kwons3327@gmail.com>
2025-12-10 00:30:54 +0900

    add: TrainingSelectionView 추가

---
+++ FindReserve/Presentation/Matching/View/TrainingSelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  TrainingSelectionView.swift
+//  FindReserve
+//
+//  Created by a on 12/10/25.
+//
+
+import SwiftUI
+
+struct TrainingSelectionView: View {
+    var body: some View {
+        VStack {
+            Text("훈련 선택")
+        }
+    }
+}
+
+#Preview {
+    TrainingSelectionView()
+}
